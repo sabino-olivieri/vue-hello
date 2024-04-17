1765aa5f0d1ccfc6991bac3886d28d45f603ec85
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-17 16:21:41 +0200

    feat: all completed

--- js/script.js
+++ js/script.js
@@ -4,11 +4,37 @@ createApp({
     data() {
         return {
 
-            
+            title: "Presentati utente",
+            username: "",
+            age: "",
+            classResult: "",
+            message: "",
+            imagePath: ""
         }
     },
 
     methods: {
+        send: function() {
 
+            if(this.username === ""){ 
+                return
+            }
+            if(this.age <= 0){
+                return
+            } 
+
+            this.classResult = "mt-2 result";
+            this.message = `Ciao ${this.username.toUpperCase()}`;
+            this.username = "";
+            this.age = ""; 
+
+            if(this.age < 18) {
+                this.imagePath = "./img/baby.svg";
+            } else if(this.age >= 40) {
+                this.imagePath = "./img/old.svg";
+            } else {
+                this.imagePath = "./img/young.svg";
+            }
+        }
     }
 }).mount('#app');
